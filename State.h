d3fb7ab5374c2b3e46f96506c2d99e0ba3e32ed8
rahulshinde <rahul_shinde8@persistent.com>
2021-09-07 17:00:07 +0530

    Version-1 Visual Studio with sqlite3

---
+++ State.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include "Assembly.h"
+
+class State{
+    private:
+        int stateId;
+        std::string stateName;
+        map<std::string,int> assemblyNameAssemblyId;
+    public :
+        map<int,Assembly *> assemblyList;       //map of AssemblyId and Assembly *
+
+    	State(int,string,
+
+            int);
+    	int getStateId();
+    	void setStateId(int);
+        std::string getStateName();
+        void addNewAssembly(Assembly &);
+        map<std::string,int> getAssemblyNameAssemblyId();
+        void setAssmblyNameAssemblyId(std::string,int);
+        void showAssemblyDetails();
+        void showAssemblyVoterList();
+};
